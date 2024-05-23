f81f87f5eb90d5b559f2a5a43e912d611cfe608c
Ruslan <rtsopanov@mail.ru>
2024-05-23 14:29:08 +0300

    OOP - hw4

---
+++ src/main/java/oop/hw4/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package oop.hw4;
+
+public class AppArrayDataException extends RuntimeException {
+    public AppArrayDataException() {
+        System.out.println("Некорректный тип данных!");
+    }
+}
---
+++ src/main/java/oop/hw4/Main.java
@@ -0,0 +1,51 @@
+package oop.hw4;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char[][] arr1 = {{'1', '2', '3', '4'}, {'5', '6', '7', '8'}, {'1', '2', '3', '4'}, {'5', '6', '7'}};
+        char[][] arr2 = {{'1', '2', '3', '4'}, {'5', '6', '7', '8'}, {'1', '2', '3', '4'}, {'5', 'R', '7', '8'}};
+        char[][] arr3 = {{'1', '2', '3', '4'}, {'5', '6', '7', '8'}, {'1', '2', '3', '4'}, {'5', '6', '7', '8'}};
+
+        try {
+            array(arr1);
+//            array(arr2);
+//            array(arr3);
+        } catch (AppArraySizeException | AppArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void array(char[][] arr) {
+        int a;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr.length != 4) {
+                    throw new AppArraySizeException();
+                }
+                if (arr[i].length != 4) {
+                    throw new AppArraySizeException();
+                }
+                try {
+                    a = Integer.parseInt(String.valueOf(arr[i][j]));
+                    sum += a;
+                } catch (RuntimeException e) {
+                    throw new AppArrayDataException();
+                }
+            }
+        }
+        System.out.println("Сумма элементов массива " + sum);
+        System.out.println("Массив соответствует размеру 4*4");
+    }
+}
+
+
+
+
+
+
+
