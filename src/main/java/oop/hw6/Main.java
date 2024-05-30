6e0251820665f952efefc8883bf647b800e92ec6
Ruslan <rtsopanov@mail.ru>
2024-05-30 19:44:57 +0300

    OOP - hw6 - feature1

--- src/main/java/oop/hw6/Main.java
+++ src/main/java/oop/hw6/Main.java
@@ -5,75 +5,34 @@ import java.util.*;
 import static oop.hw6.PhoneBook.*;
 
 
+//import static oop.hw6.PhoneBook.*;
+
+
 public class Main {
     public static void main(String[] args) {
 
-        List<PhoneBook> book = new ArrayList<>(Arrays.asList(
-                new PhoneBook("Иван", "+7-916-534-11-22"),
-                new PhoneBook("Олег", "+7-963-444-00-11"),
-                new PhoneBook("Петр", "+7-927-333-15-15"),
-                new PhoneBook("Сергей", "+7-917-222-17-17"),
-                new PhoneBook("Сергей", "+7-999-698-34-58")
-        ));
+        PhoneBook phoneBook1 = new PhoneBook("Эрнесто Че Гевара", "+7-916-534-11-22");
+        PhoneBook phoneBook2 = new PhoneBook("Иосиф Сталин", "+7-963-444-00-11");
+        PhoneBook phoneBook3 = new PhoneBook("Франклин Рузвельт", "+7-927-333-15-15");
+        PhoneBook phoneBook4 = new PhoneBook("Уинстон Черчель", "+7-917-222-17-17");
+        PhoneBook phoneBook5 = new PhoneBook("Берия Лаврентий", "+7-999-698-34-58");
 
 
 
-        add("Антон",   "+7-999-999-99-99", book);
-        add("Антон",   "+7-999-888-88-88", book);
         System.out.println();
 
-        find("Антон", book);
+        add("Антон Чехов",   "+7-999-999-99-99");
         System.out.println();
 
-        System.out.println(containsPhoneNumber("+7-999-999-99-99", book));
+        add("Берия Лаврентий", "+7-999-698-11-11");
+        System.out.println();
 
+        find("Берия Лаврентий");
+        System.out.println();
+
+        System.out.println(containsPhoneNumber("+7-999-999-99-99"));
 
     }
-
-
-
-        public static void add(String name, String phone, List<PhoneBook> list) {
-        Set<String> hashSet = new HashSet<>();
-            for (PhoneBook emp : list) {
-                hashSet.add(emp.getPhoneNumber() + " " + emp.getName());
-                hashSet.add(name + " " + phone);
-            }
-            if(!(hashSet.contains(name) && hashSet.contains(phone))){
-                list.add(new PhoneBook(name, phone));
-            }
-            System.out.println(list);
-        }
-
-
-
-
-    public static void find(String name,  List<PhoneBook> list) {
-        Map<String, String> user = new HashMap<>();
-        for (PhoneBook emp : list) {
-            user.put(emp.getPhoneNumber(), emp.getName());
-        }
-        for (Map.Entry<String, String> entry : user.entrySet()) {
-            if (name == entry.getValue()) {
-                System.out.println(entry);
-            }
-        }
-    }
-
-
-
-
-    public static boolean containsPhoneNumber (String phone, List<PhoneBook> list){
-        Map<String, String> hashSet = new HashMap<>();
-        for (PhoneBook emp : list) {
-            hashSet.put(emp.getPhoneNumber(), emp.getName());
-        }
-        if(hashSet.containsKey(phone)){
-            return true;
-        }
-        return false;
-    }
-
-
 }
 
 
--- src/main/java/oop/hw6/PhoneBook.java
+++ src/main/java/oop/hw6/PhoneBook.java
@@ -1,17 +1,24 @@
 package oop.hw6;
 
 
+import java.util.*;
+
 public class PhoneBook {
 
     String name;
     String phoneNumber;
 
+
+    public static Map<String, Set<String>> book = new HashMap<>();
+
+
     public PhoneBook(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
-
+        Set<String> phone = new HashSet<>();
+        phone.add(phoneNumber);
+        book.put(name, phone);
     }
-
 
 
     public String getName() {
@@ -31,14 +38,41 @@ public class PhoneBook {
     }
 
 
+    public static void add(String name, String phone1) {
+        Set<String> map = new HashSet<>();
 
-
-
-
-    @Override
-    public String toString() {
-        return
-                name + " " + phoneNumber;
+        for (Map.Entry<String, Set<String>> emp : book.entrySet()) {
+            if (emp.getKey() == name) {
+                map.add(emp.getValue() + " " + phone1);
+                book.put(emp.getKey(), map);
+            }
+        }
+        if (!(book.containsKey(name))) {
+            map.add(phone1);
+            book.put(name, map);
+        }
+        System.out.println(book);
     }
+
+
+    public static void find(String name) {
+        for (Map.Entry<String, Set<String>> emp : book.entrySet()) {
+            if (emp.getKey() == name) {
+                System.out.println("Имя: " + emp.getKey() + " " + "Тел: " + emp.getValue());
+            }
+        }
+    }
+
+
+    public static boolean containsPhoneNumber(String phone) {
+        Set<String> str = new HashSet<>();
+        str.add(phone);
+        if (book.containsValue(str)) {
+            return true;
+        }
+        return false;
+    }
+
 }
+
 
