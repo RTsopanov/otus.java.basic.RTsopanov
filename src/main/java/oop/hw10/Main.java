25c1d7510ee9cd200bafa09294bfe88f60e7811f
Ruslan <rtsopanov@mail.ru>
2024-06-20 10:09:38 +0300

    OOP - hw10 - feature 2

--- src/main/java/oop/hw10/Main.java
+++ src/main/java/oop/hw10/Main.java
@@ -15,7 +15,6 @@ public class Main {
         }
 
 
-
         long startTime = System.currentTimeMillis();
         myThread(array);
         long endTime = System.currentTimeMillis();
@@ -23,56 +22,60 @@ public class Main {
         System.out.println(endTime - startTime);
 
 
-
-
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                myThreadOne(array);
+                for (int i = 0; i < 25_000_000; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
             }
         });
 
-        Thread t2 = new Thread(new Runnable() {
+
+        Thread t2 = new Thread(new Runnable(){
             @Override
-            public void run() {
-                myThreadTwo(array);
+            public void run(){
+                for (int i = 25_000_000; i < 50_000_000; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
             }
         });
 
-        Thread t3 = new Thread(new Runnable() {
+        Thread t3 = new Thread(new Runnable(){
             @Override
-            public void run() {
-                myThreadThree(array);
+            public void run(){
+                for (int i = 50_000_000; i < 75_000_000; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
             }
         });
 
-        Thread t4 = new Thread(new Runnable() {
+        Thread t4 = new Thread(new Runnable(){
             @Override
-            public void run() {
-                myThreadFour(array);
+            public void run(){
+                for (int i = 75_000_000; i < 100_000_000; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
             }
         });
 
 
 
         long startTimeTwo = System.currentTimeMillis();
+
         t1.start();
-//        t1.join();
 
         t2.start();
-//        t2.join();
 
         t3.start();
-//        t3.join();
 
         t4.start();
-//        t4.join();
-
 
         long endTimeTwo = System.currentTimeMillis();
+
+
         System.out.print("\nВремя выполнения №2: ");
         System.out.println(endTimeTwo - startTimeTwo);
-
 
 
     }
--- src/main/java/oop/hw10/MyClass.java
+++ src/main/java/oop/hw10/MyClass.java
@@ -5,40 +5,10 @@ import java.util.Arrays;
 public class MyClass {
 
 
-
-
-
-    public static void myThread(double[] array){
-        for (int i = 0; i < array.length; i++) {
+    public static void myThread(double[] array) {
+        for (int i = 0; i < 100_000_000; i++) {
             array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
         }
     }
 
-
-
-    public static void myThreadOne(double[] array){
-        for (int i = 0; i < 25_000_000; i++) {
-            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-        }
-    }
-
-
-    public static void myThreadTwo (double[] array){
-        for (int i = 25_000_000; i < 50_000_000; i++) {
-            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-        }
-    }
-
-
-    public static void myThreadThree (double[] array){
-        for (int i = 50_000_000; i < 75_000_000; i++) {
-            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-        }
-    }
-
-    public static void myThreadFour (double[] array){
-        for (int i = 75_000_000; i < 100_000_000; i++) {
-            array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-        }
-    }
 }
