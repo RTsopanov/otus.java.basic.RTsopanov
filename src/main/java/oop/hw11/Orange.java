437b3a0e5e138ef74da77203536046e6af8f83b4
Ruslan <rtsopanov@mail.ru>
2024-07-09 12:11:48 +0300

    OOP - hw11

---
+++ src/main/java/oop/hw11/Orange.java
@@ -0,0 +1,19 @@
+package oop.hw11;
+
+public class Orange extends Fruit {
+    private int quantityOrange;
+
+
+    public Orange(int quantityOrange) {
+        this.quantityOrange = quantityOrange;
+    }
+
+    public int getQuantityOrange() {
+        return quantityOrange;
+    }
+
+    public void setQuantityOrange(int quantityOrange) {
+        this.quantityOrange = quantityOrange;
+
+    }
+}
