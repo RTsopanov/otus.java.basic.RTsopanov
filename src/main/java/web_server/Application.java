9ddb2e5c28bca3a589fdf6520e46066fc44f4345
Ruslan <rtsopanov@mail.ru>
2024-08-15 14:35:50 +0300

    web-server_6

--- src/main/java/web_server/Application.java
+++ src/main/java/web_server/Application.java
@@ -1,11 +1,29 @@
 package web_server;
 
 
+import java.beans.JavaBean;
+import java.util.jar.JarEntry;
+
 
 public class Application {
 
     public static void main(String[] args) {
-        new HttpServer(8189).start();
+
+        System.getProperties().put("port 1", "8185");
+        System.getProperties().put("port 2", "8186");
+
+        System.out.println(System.getProperty("user.name"));
+        new HttpServer(Integer.parseInt(System.getProperty("port 2"))).start();
+
+
+
+// TODO вариант №2 с использованием System.getProperty
+//        System.getProperties().put("port 3", 8187);
+//        new HttpServer((Integer) System.getProperties().getOrDefault("port 3", 8888)).start();
+
+
+
+
     }
 
 }
